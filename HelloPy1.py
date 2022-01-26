7442de46402bbd4d35980d0265a158e7c155531b
ankur-saxena-88 <indiarocks88@gmail.com>
2022-01-26 12:25:48 +0530

    Python first hello world program added

---
+++ HelloPy1.py
@@ -0,0 +1,11 @@
+# python hello world program.
+# @author: Ankur Saxena
+# Date: Wed, 26-Jan-2022
+# Platform: Linux Ubuntu 20.04/Lts/x64/Python 3.6/Vim
+
+# program start
+
+print ("Hi, Python World!\n")
+
+# program end
+

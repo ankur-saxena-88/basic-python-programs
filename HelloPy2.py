ba2929dfef40b37635c8c5ba70c14498f593cca5
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-01-26 13:15:17 +0530

    Python multiline string program added

---
+++ HelloPy2.py
@@ -0,0 +1,17 @@
+# Programs: Python program to print following multiline string on the console screen using single print command...
+
+"""
+Hi, Python World!
+Welcome to the world of Python...
+Let us learn Pyrhon programming language.
+"""
+# @author: Ankur Saxena
+# Date: Wed, 26-Jan-2022
+# Platform: Linux Ubuntu 20.04/Lts/x64/Python-3.6/Vim
+
+# program start
+
+print ("Hi, Python World!\nWelcome to the world of Python...\nLet us learn Python programming language.")
+
+# program end
+
